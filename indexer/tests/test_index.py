6fae5268fd2b35a1b6dec111b5e1f5e0a0ea47f8
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-04 11:56:33 +0000

    merge tests in a single class. Use utilities

--- indexer/tests/test_index.py
+++ indexer/tests/test_index.py
@@ -4,6 +4,9 @@ import json
 import subprocess
 import os.path
 
+from indexer.utilities import create_bzip2_file
+from indexer.utilities import create_hdt_file
+from indexer.utilities import remove_file
 from indexer.index import Bzip2Reader
 from indexer.index import index_hdt
 from indexer.index import get_elastic_search_props
@@ -13,31 +16,20 @@ from indexer.tests import NERTestServer
 
 logger = logging.getLogger(__name__)
 
-def create_bzip2_file(file_path):
-  if not os.path.isfile(file_path + ".tmp.bz2"): 
-    subprocess.call(["cp", file_path, file_path + ".tmp"])
-    subprocess.call(["bzip2", file_path + ".tmp"])
-  return file_path + ".tmp.bz2"
-
-def create_hdt_file(input_file_path, output_file_path):
-  if not os.path.isfile(output_file_path + ".hdt"):
-    subprocess.call(["rdf2hdt", input_file_path, output_file_path + ".hdt"])
-  return output_file_path + ".hdt"
-
-def remove_file(file_path):
-  if os.path.isfile(file_path):
-    subprocess.call(["rm", file_path])
-
-class TestBzip2Reader(unittest.TestCase):
+class TestIndexer(unittest.TestCase):
   def setUp(self):
-    pass
+    self.file_path = "etc/test.nt"
+    self.bzip2_file_path = create_bzip2_file(self.file_path)
+    self.hdt_file_path = create_hdt_file(self.bzip2_file_path, self.file_path)
 
-  def test_nextLines(self):
-    file_path = "etc/test.nt"
-    bzip2_file_path = create_bzip2_file(file_path)
+  def tearDown(self):
+    remove_file(self.bzip2_file_path)
+    remove_file(self.hdt_file_path)
+
+  def test_bzip2_reader(self):
     totalLines = 0
     try:
-      f = Bzip2Reader(bzip2_file_path, "r", 10)
+      f = Bzip2Reader(self.bzip2_file_path, "r", 10)
       lines = f.nextLines()
       totalLines = len(lines)
       while lines :
@@ -47,28 +39,17 @@ class TestBzip2Reader(unittest.TestCase):
     except Exception as e:
       logger.error("Exception while reading")
       logger.error(e)
-    remove_file(bzip2_file_path)
     self.assertEqual(totalLines, 100) 
 
-class TestIndexer(unittest.TestCase):
-  def setUp(self):
-    pass
-
   def test_index_hdt(self):
-    logger.info("asd")
-    file_path = "etc/test.nt"
     index_name = "test"
     index_header = { "index" : { "_index": index_name, "_type": "triple" }}
     buffer_size = 10
     num_threads = 1
     with ElasticSearchTestServer(port = 9500) as ests, NERTestServer(port = 9600) as nts:
-      bzip2_file_path = create_bzip2_file(file_path)
-      hdt_file_path = create_hdt_file(bzip2_file_path, file_path)
       es = ElasticSearch(ests.get_url())
       es.create_index(index_name, get_elastic_search_props() )
-      n = index_hdt(hdt_file_path, es, index_header, buffer_size, nts.get_url(), num_threads)
+      n = index_hdt(self.hdt_file_path, es, index_header, buffer_size, nts.get_url(), num_threads)
       es.delete_index(index_name)
-      remove_file(bzip2_file_path)
-      remove_file(hdt_file_path)
     self.assertEqual(19, n)
     
