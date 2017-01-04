d24406393c498511e755066dc3b5f9526d09ff76
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-04 10:43:18 +0000

    merge both tests to avoid delays in the initialisation of ES

--- indexer/tests/test_wbservice.py
+++ indexer/tests/test_wbservice.py
@@ -13,23 +13,17 @@ class TestElasticSearch(unittest.TestCase):
   def setUp(self):
     pass
 
-  def test_create_delete_index(self):
-    print(es_server)
-    ES = ElasticSearch(es_server)
-    ES.create_index('test', index_props)
-    ES.delete_index('test') 
-
-  def test_bulk(self):
-    try:
-      with ElasticSearchTestServer(port = 9500) as ts:
-        ES = ElasticSearch(ts.get_url())
-        ES.create_index('test', index_props)
-        b_1 = { "index": { "_index": "test", "_type": "triple" }}
-        s_1 = {"title":"myres1"}
-        s_2 = {"title":"myres2"}
-        contents = [s_1, s_2]
-        data = create_package(b_1, contents)
-        ES.bulk(data)
-        ES.delete_index('test')
-    except Exception as e:
-      logger.error(e)
+  def test_client(self):
+    with ElasticSearchTestServer(port = 9500) as ts:
+      # Create index
+      ES = ElasticSearch(ts.get_url())
+      ES.create_index('test', index_props)
+      # Bulk
+      b_1 = { "index": { "_index": "test", "_type": "triple" }}
+      s_1 = {"title":"myres1"}
+      s_2 = {"title":"myres2"}
+      contents = [s_1, s_2]
+      data = create_package(b_1, contents)
+      ES.bulk(data)
+      # delete index
+      ES.delete_index('test')
