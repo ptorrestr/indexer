8ed7f339ab99198d8531e710d45bf8545fa1d3e1
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-30 15:25:49 +0000

    change vm size

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -8,10 +8,12 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 else
   os="Linux"
   # Install docker
-  sudo ls /etc/docker/
   sudo mkdir -p /etc/docker/certs.d/$DOCKER_URL 
   sudo wget $DOCKER_CERT_URL -O /etc/docker/certs.d/$DOCKER_URL/ca.crt
   sudo service docker restart
+  # force vm size for elasticsearch
+  # See https://www.elastic.co/guide/en/elasticsearch/reference/5.0/vm-max-map-count.html#vm-max-map-count
+  sudo sysctl -w vm.max_map_count=262144
 fi
 
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
