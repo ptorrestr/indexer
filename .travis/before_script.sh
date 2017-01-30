fb6418080128facfe46c7a666fb24c348d9a20b7
Pablo Torres <pablo.torres@insight-centre.org>
2017-01-30 15:16:48 +0000

    test elasticsearch

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -10,9 +10,10 @@ echo "export NER_URL=\"http://$(hostname):8080/ner\"" > .env
 echo "export ELASTICSEARCH_URL=\"http://$(hostname):9200\"" >> .env
 echo "export ELASTICSEARCH_USER=elastic" >> .env
 echo "export ELASTICSEARCH_PASSWORD=changeme" >> .env
-docker run -p 9200:9200 \
+docker run -d -p 9200:9200 \
 	-v testesdata:/usr/share/elasticsearch/data \
 	docker.elastic.co/elasticsearch/elasticsearch:5.1.1 >> .containers
+cat .containers | xargs docker logs -f
 docker run -d -p 8080:8080 \
 	$DOCKER_URL/ner >> .containers
 docker ps
