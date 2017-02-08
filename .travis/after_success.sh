0be3d8b05d566179db0002ad940bbe7b7e7bca04
Pablo Torres <pablo.torres@insight-centre.org>
2017-02-08 11:25:25 +0000

    publish only when master is built

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,7 +2,10 @@
 
 set -x -e
 
-export PATH="$HOME/miniconda/bin:$PATH"
-source activate indexer-test
-file=$(conda build .conda/ --output)
-anaconda -t $ANACONDA_TOKEN upload $file -u $ANACONDA_USER --force
+# Only publish when master is build
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  export PATH="$HOME/miniconda/bin:$PATH"
+  source activate indexer-test
+  file=$(conda build .conda/ --output)
+  anaconda -t $ANACONDA_TOKEN upload $file -u $ANACONDA_USER --force
+fi
