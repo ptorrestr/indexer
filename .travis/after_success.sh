f92c70a28c878680ff94eccd54ed6875fcbc84ce
Pablo Torres <pablo.torres@insight-centre.org>
2017-02-08 11:20:02 +0000

    use proper environment when successed

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -3,6 +3,6 @@
 set -x -e
 
 export PATH="$HOME/miniconda/bin:$PATH"
-source activate test-environment
+source activate indexer-test
 file=$(conda build .conda/ --output)
 anaconda -t $ANACONDA_TOKEN upload $file -u $ANACONDA_USER --force
